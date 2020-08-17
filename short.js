ad308c02c73b29eeb709a0c451069629f40169fb
Sarita <sarita@something.com>
2020-08-17 11:15:54 -0700

    first commit

---
+++ short.js
@@ -0,0 +1,15 @@
+  
+  function Cell (parentArr ){
+    for(var i=0; i<parentArr.length; i++){
+   for(var j=0; j<parentArr[i].length; j++){
+   
+    var childArr=parentArr[i];
+    childArr[4]> parentArr[i+1][4]
+      console.log(" check ")
+      
+    }
+   }
+ } 
+
+
+ 
